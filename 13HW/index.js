a428bfc755faf60832027a269e9e86588fc694d4
vectorov-edu <vectorov.edu@gmail.com>
2019-01-23 09:44:35 +0200

    Add the third exercise: send email using nodejs

--- 13HW/index.js
+++ 13HW/index.js
@@ -15,6 +15,47 @@
 // writeFile('New string3 \n', true);
 // readFileToConsole();
 
+/*
+ **3. Отправить себе на почту письмо с помощью Node.js
+*/
+
+const nodemailer = require('nodemailer');
+
+nodemailer.createTestAccount(function(err, account) {
+	if (err) {
+		console.error('Failed to create a testing account. ' + err.message);
+		return process.exit(1);
+	}
+console.log(account);
+	// create reusable transporter object using the default SMTP transport
+	const transporter = nodemailer.createTransport({
+		host: account.smtp.host,
+		port: account.smtp.port,
+		secure: account.smtp.secure,
+		auth: {
+			user: account.user, // generated ethereal user
+			pass: account.pass // generated ethereal password
+		}
+	});
+		
+	const message = {
+		from: 'mail@example.com',
+		to: 'mail@example.com',
+		subject: 'Sending Email from me',
+		text: 'Hello to myself!',
+		html: '<p><b>Hello</b> to myself!</p>'
+	};
+
+	transporter.sendMail(message, function(error, info){
+		if (error) {
+			console.log(error);
+		} else {
+			console.log('Email sent: ' + info.response);
+			console.log(info);
+			console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info))
+		}
+	});
+});
 
 //==========================================================================
 function readFileToConsole() {
