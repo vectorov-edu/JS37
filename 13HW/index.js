7533f216b599e9d009129ff5bda6e45d808ebea8
vectorov-edu <vectorov.edu@gmail.com>
2019-01-23 09:42:55 +0200

    Add the first execise: read/write data to file by nodejs

---
+++ 13HW/index.js
@@ -0,0 +1,24 @@
+﻿const fs = require('fs');
+
+/*
+ 1. С помощью модуля fs научится записывать данные в файл, а также читать из файла.
+*/
+// readFileToConsole();
+// writeFile('New string2 \n');
+// readFileToConsole();
+
+
+//==========================================================================
+function readFileToConsole() {
+	fs.readFile('demo_data.txt', 'utf8', function(err, data) {
+		if(err) throw err;
+		console.log(data);
+	});
+}
+
+function writeFile(dataToWrite, isAppending) {
+	const flag = isAppending ? {flag: 'a'} : undefined;
+	fs.writeFile('demo_data.txt', dataToWrite, flag, function(err) {
+		if(err) throw err;
+	});
+}
